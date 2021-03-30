80f107e8e23807541101938b8a40fdaa693d7968
Rahul Sharma <90137rahul@gmail.com>
2021-03-30 14:20:37 +0530

    🍻 Changes required

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Discord, { MessageEmbed } from "discord.js";
+import Discord, { Guild, MessageEmbed, MessageMentions } from "discord.js";
 //import ms from "ms";
 import * as dotenv from "dotenv";
 const client = new Discord.Client();
@@ -51,14 +51,47 @@ client.on("message", (message) => {
         .setTitle("User Information")
         .addField("Player Name ", message.author.username, true)
         .addField("version is ", version, true)
-        .addField("Current server", message.guild?.name,true)
+        .addField("Current server", message.guild?.name, true)
         .setColor("#F1C40f")
         .setThumbnail(message.author.displayAvatarURL())
         .setFooter("Made with 💕 by Rahul Sharma, Riju, and Bhavya");
       message.channel.send({ embed });
       break;
+    case "mute":
+      // let mentioned: Discord.User = message.mentions.users.first() || args[1];
+      // let person = message.guild?.members.fetch(mentioned);
+      // let mainrole = message.guild?.roles.cache.find(
+      //   (role) => role.name === "Newbie"
+      // );
+      // let muterole = message.guild?.roles.cache.find(
+      //   (role) => role.name === "Mute"
+      // );
+
+      // if (!muterole) return message.reply("Couldn't find mute role");
+      // let time = args[2];
+      // if (!time) {
+      //   return message.reply(" you have to provide time ⌚ as 3rd argument");
+      // }
+      // if (!person) {
+      //   return message.reply("Please mention the person you want to mute");
+      // } else {
+      //   person.removerole()
+      // }
+      let target = message.mentions.users.first();
+      if (target) {
+        let mainRole = message.guild?.roles.cache.find(
+          (role) => role.name === "Newbie"
+        );
+        let muteRole = message.guild?.roles.cache.find(
+          (role) => role.name === "Mute"
+        );
+        let memberTarget = message.guild?.members.cache.get(target.id);
+        memberTarget?.roles.remove(mainRole?.id);
+      } else {
+        message.reply("Can't find that user 👾");
+      }
+
+      break;
   }
 });
 client.login(process.env.TOKEN);
-
-
